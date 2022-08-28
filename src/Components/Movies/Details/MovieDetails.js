da7d3aacd18fc6b11901c9a71974429939c53779
Samarth Keskar <samarth@uable.com>
2022-08-28 10:11:40 +0530

    movie details page done

--- src/Components/Movies/Details/MovieDetails.js
+++ src/Components/Movies/Details/MovieDetails.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "./MovieDetails.css";
+import { useParams, Link } from "react-router-dom";
+import useDebouncedFetch from "../../../Hooks/useDebouncedFetch";
 
 const MovieDetails = () => {
-  return <h3>hello</h3>;
+  const { id } = useParams();
+  const { isLoading, error, data: movie } = useDebouncedFetch(`&i=${id}`, true);
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  if (error.show) {
+    return (
+      <div className="page-error">
+        <h1>{error.msg}</h1>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    );
+  }
+  const { Poster: poster, Title: title, Plot: plot, Year: year } = movie;
+  return (
+    <section className="movie-details">
+      <img src={poster} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>{plot}</p>
+        <h4>{year}</h4>
+        <Link to="/" className="btn">
+          back to movies
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default MovieDetails;
